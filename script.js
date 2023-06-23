3cacc33e1ba5ccde8256a2a2c40453e6e3dfc2d5
teyyubmammadovh <teyyubmammadovh@gmail.com>
2023-06-23 16:18:24 +0400

    full responsive section

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+// AOS Animation
 AOS.init();
 
+
+const myIcon = document.getElementById('youtube-icon')
+const videoLink = document.getElementById('video-link')
+
+myIcon.addEventListener("click", (e)=> {
+   e.preventDefault()
+   videoLink.classList.toggle('display-on')
+   myIcon.classList.toggle('fa-circle-pause')
+   
+})
 
 
 
